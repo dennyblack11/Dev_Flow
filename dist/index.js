929a072fa1578b1231e7231ba4bffd2b09e74bf4
dennyblack11 <dennisozoemena3@gmail.com>
2024-01-09 09:05:48 +0100

    building the first code for staging

--- dist/index.js
+++ dist/index.js
@@ -6,16 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const dbConfig_1 = require("./utils/dbConfig");
+const mainApp_1 = require("./mainApp");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = parseInt(process.env.PORT);
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+(0, mainApp_1.mainApp)(app);
 const server = app.listen(port, () => {
-    console.clear();
-    console.log();
-    (0, dbConfig_1.dbConfig)();
+    console.log("connected");
 });
 process.on("uncaughtException", (err) => {
     console.log("uncaughtException: ", err);
